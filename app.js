2208f7b2ffa1f8749f0ec22286650e9a44a6e472
Robin <19738286+cawc@users.noreply.github.com>
2020-10-02 20:21:28 +0200

    small changes cors

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.disable('etag')
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
   next()
 })
 
