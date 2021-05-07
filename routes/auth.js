32e3228f130e1237d722ebed1f8b16bc0bfad802
Robin <19738286+cawc@users.noreply.github.com>
2021-05-07 16:58:26 +0200

    add refresh tokens, auth done

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,12 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const db = require('../db')
 const users = db.get('users')
-const userSchema = require('../user-schema')
+const refreshTokens = db.get('refreshtokens')
+const userRegistrationSchema = require('../user-registration-schema')
 const utils = require('../utils')
 const bcrypt = require('bcrypt')
 
-router.get('/', async (req, res, next) => {
+router.get('/', utils.authJWT, utils.adminOnly, async (req, res, next) => {
   try {
     const userlist = await users.find()
     res.json(userlist)
@@ -16,20 +17,22 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/login', async (req, res, next) => {
+router.post('/authenticate', async (req, res, next) => {
   try {
     const { username, password } = req.body
     const user = await users.findOne({ username: username })
     if (!user) {
       res.status(401).send('Username/password incorrect')
     } else {
-      bcrypt.compare(password, user.passhash, (err, match) => {
+      bcrypt.compare(password, user.passhash, async (err, match) => {
         if (err) {
           next(err)
         }
         if (match) {
-          const accessToken = jwt.sign({ username: user.username, role: user.role }, process.env.JWT_SECRET)
-          res.json({ accessToken })
+          const accessToken = jwt.sign({ username: user.username, role: user.role }, process.env.JWT_SECRET, { expiresIn: '30m' })
+          const refreshToken = jwt.sign({ username: user.username, role: user.role }, process.env.JWT_REFRESH_SECRET)
+          await refreshTokens.insertOne({ userid: user._id, token: refreshToken })
+          res.json({ accessToken, refreshToken })
         } else {
           res.status(401).send('Username/password incorrect')
         }
@@ -40,9 +43,38 @@ router.get('/login', async (req, res, next) => {
   }
 })
 
-router.post('/register', async (req, res, next) => {
+router.post('/token', async (req, res, next) => {
   try {
-    let user = await userSchema.validateAsync(req.body)
+    const { token } = req.body
+    if (!token) res.sendStatus(401)
+    if (await refreshTokens.count({ token: token }) < 1) res.sendStatus(403)
+
+    jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, user) => {
+      if (err) {
+        next(err)
+      } else {
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, process.env.JWT_SECRET, { expiresIn: '30m' })
+        res.json({ accessToken })
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/logout', utils.authJWT, async (req, res, next) => {
+  try {
+    const { token } = req.body
+    await refreshTokens.remove({ token: token })
+    res.send('Logout success')
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/register', utils.authJWT, utils.adminOnly, async (req, res, next) => {
+  try {
+    let user = await userRegistrationSchema.validateAsync(req.body)
     const nameCheck = await users.findOne({ username: user.username })
     if (nameCheck) {
       res.status(409).send('User with this username already exists')
@@ -66,7 +98,7 @@ router.post('/register', async (req, res, next) => {
   }
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', utils.authJWT, utils.adminOnly, async (req, res, next) => {
   try {
     const { id } = req.params
     await users.remove({ _id: id })
